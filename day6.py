f63b2fd9d373075f3e7916b6f29802e752a3c643
HarshMarthak <harsh.marthak@gmail.com>
2022-04-01 15:50:55 +0530

    Solved!

--- day6.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def countOrders(self, n: int) -> int:
-        mod=1000000007
-        if n==0:
-            return 1
-        ans=n*(2*n-1)%mod
-        ans=ans * Solution.countOrders(self,n-1) % mod
-        return ans
