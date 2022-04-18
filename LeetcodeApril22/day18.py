fb8781faae37bf4b40b2d9cc85fdf1f7698e7eb0
HarshMarthak <harsh.marthak@gmail.com>
2022-04-18 15:11:06 +0530

    Solved!

---
+++ LeetcodeApril22/day18.py
@@ -0,0 +1,29 @@
+#iterative
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        stack = []
+        while root or stack:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            k -= 1
+            if k == 0:
+                return root.val
+            root = root.right
+#recursive
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        self.k = k
+        self.res = None
+        self.helper(root)
+        return self.res
+    def helper(self, node):
+        if not node:
+            return
+        self.helper(node.left)
+        self.k -= 1
+        if self.k == 0:
+            self.res = node.val
+            return
+        self.helper(node.right)
