c5053d3abde4a940192c4e4728c07ec8edd29fd3
HarshMarthak <harsh.marthak@gmail.com>
2022-09-03 19:57:35 +0530

    Solved!

---
+++ LeetcodeSeptember22/day3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numsSameConsecDiff(self, n: int, k: int) -> List[int]:
+        numset = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        for i in range(n - 1):
+            res = []
+            for num in numset:
+                cur = num % 10
+                if cur + k <= 9: res.append(num * 10 + cur + k)
+                if k != 0 and cur - k >= 0: res.append(num * 10 + cur - k)
+            numset = res
+        return res
